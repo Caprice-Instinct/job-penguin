a8afa2345877d1bf4d7fb015a7e276c4409725f0
Caprice-Instinct <linetwangui04@gmail.com>
2025-03-17 17:50:26 +0300

    arcjet setup

--- app/actions.ts
+++ app/actions.ts
@@ -1,53 +1,83 @@
-"use server"
+"use server";
 
-import { z } from "zod"
-import { requireUser } from "./utils/requireuser"
-import { companySchema, jobSeekerSchema } from "./utils/zodSchemas"
-import { prisma } from "./utils/db"
-import { redirect } from "next/navigation"
+import { z } from "zod";
+import { requireUser } from "./utils/requireuser";
+import { companySchema, jobSeekerSchema } from "./utils/zodSchemas";
+import { prisma } from "./utils/db";
+import { redirect } from "next/navigation";
+import arcjet, { detectBot, shield } from "./utils/arcjet";
+import { request } from "@arcjet/next";
 
-export async function createCompany(data:z.infer<typeof companySchema>){
-    const session = await requireUser()
-
-    const validateData = companySchema.parse(data)
-
-    await prisma.user.update({
-        where: {
-            id: session.id
-        },
-        data: {
-            onboardingCompleted: true,
-            userType: "COMPANY",
-            Company: {
-                create: {
-                    ...validateData
-                }
-            }
-        }
-    });
-
-    return redirect("/")
-}   
-
-
-export async function createJobSeeker(data: z.infer<typeof jobSeekerSchema>){
-    const user = await requireUser()
-
-    const validateData = jobSeekerSchema.parse(data);
-
-    await prisma.user.update({
-        where: {
-            id: user.id
-        },
-        data: {
-            onboardingCompleted: true,
-            userType: "JOB_SEEKER",
-            JobSeeker: {
-                create: {
-                    ...validateData
-                }
-            }
-        }
+const aj = arcjet
+  .withRule(
+    shield({
+      mode: "LIVE",
     })
-    return redirect("/")
+  )
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: [],
+    })
+  );
+
+export async function createCompany(data: z.infer<typeof companySchema>) {
+  const session = await requireUser();
+
+  //   Arcjet protection
+  const req = await request();
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    throw new Error("Forbidden");
+  }
+
+  const validateData = companySchema.parse(data);
+
+  await prisma.user.update({
+    where: {
+      id: session.id,
+    },
+    data: {
+      onboardingCompleted: true,
+      userType: "COMPANY",
+      Company: {
+        create: {
+          ...validateData,
+        },
+      },
+    },
+  });
+
+  return redirect("/");
+}
+
+export async function createJobSeeker(data: z.infer<typeof jobSeekerSchema>) {
+  const user = await requireUser();
+
+  //   Arcjet protection
+  const req = await request();
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    throw new Error("Forbidden");
+  }
+
+  const validateData = jobSeekerSchema.parse(data);
+
+  await prisma.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      onboardingCompleted: true,
+      userType: "JOB_SEEKER",
+      JobSeeker: {
+        create: {
+          ...validateData,
+        },
+      },
+    },
+  });
+  return redirect("/");
 }
