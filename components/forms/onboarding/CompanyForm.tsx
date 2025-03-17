4d3273a1576d034f30d37c5c7c5e79d109b58e03
Caprice-Instinct <linetwangui04@gmail.com>
2025-03-17 06:07:46 +0300

    upload logo with uploadthing

--- components/forms/onboarding/CompanyForm.tsx
+++ components/forms/onboarding/CompanyForm.tsx
@@ -22,6 +22,7 @@ import {
 } from "@/components/ui/select";
 import { countryList } from "@/app/utils/countriesList";
 import { Textarea } from "@/components/ui/textarea";
+import { UploadDropzone } from "@/components/general/UploadThingReexported";
 
 export function CompanyForm() {
   const form = useForm<z.infer<typeof companySchema>>({
@@ -124,6 +125,28 @@ export function CompanyForm() {
                 <FormLabel>About</FormLabel>
                 <FormControl>
                 <Textarea placeholder="Tell us about your company" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="logo"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Company Logo</FormLabel>
+                <FormControl>
+                <UploadDropzone
+                      endpoint="imageUploader"
+                      onClientUploadComplete={(res) => {
+                        field.onChange(res[0].url);
+                      }}
+                      onUploadError={() => {
+                        console.error("Something went wrong. Please try again.");
+                      }}
+                      className="ut-button:bg-primary ut-button:text-white ut-button:hover:bg-primary/90 ut-label:text-muted-foreground ut-allowed-content:text-muted-foreground border-primary"
+                    />
                 </FormControl>
                 <FormMessage />
               </FormItem>
